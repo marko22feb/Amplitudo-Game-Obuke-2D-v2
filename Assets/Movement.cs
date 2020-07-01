f2e9f80a695e9b6e16936bb3d4aa784ee8df6c6d
RoiaLaptop <45495716+UnderdogTagTeam@users.noreply.github.com>
2020-07-01 17:59:40 +0200

    Drugi Cas

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     Transform tr;
     Rigidbody2D rd2D;
+    BoxCollider2D boxCollider;
+    public LayerMask layer = default;
     public float Speed = 0;
     public float JumpHeight = 2;
 
@@ -13,27 +15,38 @@ public class Test : MonoBehaviour
     {
         tr = GetComponent<Transform>();
         rd2D = GetComponent<Rigidbody2D>();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-     
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
+    bool IsOnGround()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 1.1f, layer);
+        if (hit.collider != null) { 
+            if (hit.collider.gameObject.layer == 10)
+            {
+                return true;
+            } else return false;
+        }
+        return false;
+    }
+
     void Update()
     {
-        Debug.Log(Speed * Time.deltaTime);
+        IsOnGround();
         if (Input.GetAxis("Horizontal") != 0)
         {
             Vector3 Velocity = new Vector3(rd2D.velocity.x + Speed * Input.GetAxis("Horizontal")  * Time.deltaTime, rd2D.velocity.y, 0);
             rd2D.velocity = Velocity;
         }
-
-        if (Input.GetButtonDown("Jump")) 
+        if (IsOnGround())
         {
-            Vector3 Velocity = new Vector3(rd2D.velocity.x, rd2D.velocity.y + JumpHeight, 0);
-            rd2D.velocity = Velocity;
+            if (Input.GetButtonDown("Jump"))
+            {
+                {
+                    Vector3 Velocity = new Vector3(rd2D.velocity.x, rd2D.velocity.y + JumpHeight, 0);
+                    rd2D.velocity = Velocity;
+                }
+            }
         }
     }
 
