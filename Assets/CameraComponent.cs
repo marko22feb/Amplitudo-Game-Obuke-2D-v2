41c97e839f694c215f1b8869cbca7e7c524bab94
RoiaLaptop <45495716+UnderdogTagTeam@users.noreply.github.com>
2020-07-03 17:46:15 +0200

    Treci Cas

---
+++ Assets/CameraComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraComponent : MonoBehaviour
+{
+    Transform playerTransform;
+    public Vector3 offset;
+
+    private void Awake()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = playerTransform.position + offset;
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,6 +8,8 @@ public class Movement : MonoBehaviour
     Rigidbody2D rd2D;
     BoxCollider2D boxCollider;
     public LayerMask layer = default;
+    GameObject floor;
+    Animator anim;
     public float Speed = 0;
     public float JumpHeight = 2;
 
@@ -16,28 +18,23 @@ public class Movement : MonoBehaviour
         tr = GetComponent<Transform>();
         rd2D = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        floor = GameObject.Find("FloorHolder");
     }
 
     bool IsOnGround()
     {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 1.1f, layer);
-        if (hit.collider != null) { 
-            if (hit.collider.gameObject.layer == 10)
-            {
-                return true;
-            } else return false;
-        }
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.2f, layer);
+        if (hit.collider != null) { anim.SetBool("IsOnGround", true); return true; }
+        anim.SetBool("IsOnGround", false);
         return false;
     }
 
     void Update()
     {
-        IsOnGround();
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            Vector3 Velocity = new Vector3(rd2D.velocity.x + Speed * Input.GetAxis("Horizontal")  * Time.deltaTime, rd2D.velocity.y, 0);
-            rd2D.velocity = Velocity;
-        }
+
+        Move(Input.GetAxis("Horizontal"));
+
         if (IsOnGround())
         {
             if (Input.GetButtonDown("Jump"))
@@ -45,9 +42,26 @@ public class Movement : MonoBehaviour
                 {
                     Vector3 Velocity = new Vector3(rd2D.velocity.x, rd2D.velocity.y + JumpHeight, 0);
                     rd2D.velocity = Velocity;
+                    anim.SetTrigger("DidJump");
                 }
             }
         }
+    }
+
+    private void Move(float value)
+    {
+        if (value != 0) {
+            Vector3 Velocity = new Vector3(rd2D.velocity.x + Speed * Input.GetAxis("Horizontal") * Time.deltaTime, rd2D.velocity.y, 0);
+            rd2D.velocity = Velocity;
+            anim.SetBool("IsMoving", true);
+
+            if (value < 0)
+            {
+                tr.localRotation = new Quaternion(0, 180, 0, 0);
+            }
+            else tr.localRotation = new Quaternion(0, 0, 0, 0);
+        }
+        else anim.SetBool("IsMoving", false);
     }
 
     private void FixedUpdate()
